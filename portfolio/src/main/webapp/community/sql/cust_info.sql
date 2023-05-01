1099280890be70445cc52e56ff0816bfa4e1f781
박정민 <@DESKTOP-NEF6FS8>
2023-05-02 02:04:32 +0900

    05/01 살려줘

---
+++ portfolio/src/main/webapp/community/sql/cust_info.sql
@@ -0,0 +1,10 @@
+insert into cust_info (cust_id,cust_pw,cust_name,cust_birth,cust_adr,cust_sex,cust_phn,cust_email,cust_pic) 
+values ("sara","sara","saraaaaa","2020-06-04","경기도","m","01022224444","sdf@sdf","info002");
+insert into cust_info (cust_id,cust_pw,cust_name,cust_birth,cust_adr,cust_sex,cust_phn,cust_email,cust_pic) 
+values ("amy","amy","amyyy","2020-06-04","전라남도","m","01022224444","sdf@sdf","info003");
+insert into cust_info (cust_id,cust_pw,cust_name,cust_birth,cust_adr,cust_sex,cust_phn,cust_email,cust_pic) 
+values ("jessie","jessie","jessieeee","2020-06-04","서울시","m","01022224444","sdf@sdf","info001");
+insert into cust_info (cust_id,cust_pw,cust_name,cust_birth,cust_adr,cust_sex,cust_phn,cust_email,cust_pic) 
+values ("sam","sam","sammmmm","2020-06-04","경기도","f","01022224444","sdf@sdf","info004");
+insert into cust_info (cust_id,cust_pw,cust_name,cust_birth,cust_adr,cust_sex,cust_phn,cust_email,cust_pic) 
+values ("hobi","hobi","hobiiii","2020-06-04","서울시","f","01022224444","sdf@sdf","info005");
---
+++ portfolio/src/main/webapp/community/sql/delete.sql
@@ -0,0 +1,4 @@
+set sql_safe_updates=0;
+SET foreign_key_checks = 0;    
+delete from
+SET foreign_key_checks = 1; 
